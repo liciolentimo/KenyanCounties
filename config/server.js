d3f0797dd520dde30245cc343bc74d5c3681c803
liciolentimo <lentimo@gmail.com>
2020-11-19 14:36:45 +0300

    installed strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6718808f3e88b7c89da247fad6a0840c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '52825bfb-0884-4f55-bafe-f898d1e64f89'
+};
